5600c60c421769f4a2587820d9721989cb502c04
Can Taş <firatcantass@gmail.com>
2022-11-05 11:11:00 +0300

    Add project files.

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context db = new Context();
+        public void AddCategory(Category category)
+        {
+            db.Add(category);
+            db.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            db.Remove(category);
+            db.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return db.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return db.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            db.Update(category);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {   
+        Context _db = new Context();
+        public void Delete(T t)
+        {
+          _db.Remove(t);
+          _db.SaveChanges();
+            
+        }
+
+        public T GetById(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _db.Add(t);
+            _db.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _db.Update(t);
+            _db.SaveChanges();
+        }
+    }
+}
